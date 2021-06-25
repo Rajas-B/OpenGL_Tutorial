61b52a4c4d3f46230e54aa337fedc12ca8402c5d
Rajas-B <bondalerajas@gmail.com>
2021-06-25 13:11:35 +0530

    Working on Test Framework

---
+++ Project1/src/Tests/TestClearColor.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Test.h"
+namespace test {
+	class TestClearColor : public Test {
+	public:
+		TestClearColor();
+		~TestClearColor();
+
+		void OnUpdate(float deltaTime) override;
+		void OnRender() override;
+		void OnImguiRender() override;
+	private:
+		float m_ClearColor[4];
+	};
+
+}
